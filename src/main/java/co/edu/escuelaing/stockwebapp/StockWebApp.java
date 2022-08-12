66fecca2a4152747a4a27856341c75bb2442524f
clases clases <clases@labinfo.is.escuelaing.edu.co>
2022-08-12 14:03:08 -0500

    Formulario API

---
+++ src/main/java/co/edu/escuelaing/stockwebapp/StockWebApp.java
@@ -0,0 +1,22 @@
+package co.edu.escuelaing.stockwebapp;
+
+import static spark.Spark.*;
+
+public class StockWebApp {
+    public static void main(String[] args) {
+        port(getPort());
+        staticFiles.location("/public");
+        get("/hello", (req, res) -> "Hello " + req.queryParams("name"));
+        get("/stock", (req, res) -> {
+            res.type("application/json");
+            return ApiReader.getStock();
+        });
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; //returns default port if heroku-port isn't set(i.e. on localhost)
+    }
+}
